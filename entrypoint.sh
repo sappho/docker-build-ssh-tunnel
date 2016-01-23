9a371abcec07083df656fd6a56d5dc8f36d8cb2e
Andrew Heald <andrew@heald.co.uk>
2016-01-23 22:22:30 +0000

    Fix service binding and remove unused switches.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 chown -R ssh-tunnel.ssh-tunnel /home/ssh-tunnel/.ssh
-su ssh-tunnel -c "autossh -M $MONITOR -nNTx -L 11111:localhost:$PORT $DEST"
+su ssh-tunnel -c "autossh -M $MONITOR -L 0.0.0.0:11111:localhost:$PORT $DEST"
