eee80af1ebc0d8bf62ef2fa5525fc7b8d8416c79
Andrew Heald <andrew@heald.co.uk>
2016-01-23 13:54:35 +0000

    Basic initial setup.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 chown -R ssh-tunnel.ssh-tunnel /home/ssh-tunnel/.ssh
-su ssh-tunnel -c "autossh -M 20000 -nNTx -L 11111:localhost:$PORT $DEST"
+su ssh-tunnel -c "autossh -M $MONITOR -nNTx -L 11111:localhost:$PORT $DEST"
