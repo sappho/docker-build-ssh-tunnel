5a04b938d5aa18588b0f74c9008def9d08e95fa4
Andrew Heald <andrew@heald.co.uk>
2016-01-24 21:52:03 +0000

    Confirm run-time command.

--- base/entrypoint.sh
+++ base/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 chmod 600 /root/.ssh/*
+echo Check SSH file permissions ...
 ls -l /root/.ssh/
+echo Running: autossh -M $MONITOR -nNTx $PROXY $DEST
 autossh -M $MONITOR -nNTx $PROXY $DEST
