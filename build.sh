f135e496537f587c373fba376d6a76ce7607e795
Andrew Heald <andrew@heald.co.uk>
2016-01-24 01:44:29 +0000

    Remove sudo from build script.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker build -t sappho/ssh-tunnel base
-sudo docker build -t sappho/ssh-tunnel-ldap ldap
+docker build -t sappho/ssh-tunnel base
+docker build -t sappho/ssh-tunnel-ldap ldap
